898bd29c087669b282fe4a3818b1df5cac1f2516
Instinct <26149719+InstinctEx@users.noreply.github.com>
2021-03-30 20:02:31 +0300

    Add files via upload

---
+++ common/install.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+ui_print " "
+ui_print "    ╭━━┳╮╭╮╱╱╱╱╭━━╮ "
+ui_print "    ┃╭╮┃┣╋╋┳┳━╮┃━━╋┳┳━┳━┳┳╮ "
+ui_print "    ┃┣┫┃━┫┃╭┫╋╰╋━━┃┃┃╋┃┻┫╭╯ "
+ui_print "    ╰╯╰┻┻┻┻╯╰━━┻━━┻━┫╭┻━┻╯ "
+ui_print "    ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯ "
+ui_print " "
+if [ -f $MODPATH/parts.tar.xz ]; then
+ tar -xf $MODPATH/parts.tar.xz -C $MODPATH/system 2>/dev/null
+ rm -f $MODPATH/parts.tar.xz 2>/dev/null
+fi;
